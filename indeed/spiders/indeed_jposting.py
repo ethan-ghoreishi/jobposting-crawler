ef40ab82f82575f96df51054f2e7050d3cba4529
Ethan Ghoreishi <25508304+ethan-ghoreishi@users.noreply.github.com>
2019-08-07 15:36:30 +0100

    Update indeed_jposting.py

--- indeed/spiders/indeed_jposting.py
+++ indeed/spiders/indeed_jposting.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#from scrapy.selector import HtmlXPathSelector
 from scrapy import Selector
 from scrapy.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
@@ -15,7 +14,6 @@ class IndeedJpostingSpider(CrawlSpider):
     name = 'indeed_jposting'
     allowed_domains = ['indeed.com']
     start_urls = ["http://www.indeed.com/jobs?q=linux&l=Chicago&sort=date?",]
-#    start_urls = ["https://www.indeed.com/jobs?q=PhD&l=united+states&rq=1&fromage=last"]
 
     rules = ( 
 		Rule(LinkExtractor(allow=('/jobs.q=linux&l=Chicago&sort=date$','q=linux&l=Chicago&sort=date&start=[0-9]+$',),deny=('/my/mysearches', '/preferences', '/advanced_search','/my/myjobs')), callback='parse_item', follow=True),
@@ -26,7 +24,6 @@ class IndeedJpostingSpider(CrawlSpider):
 #    rules = (Rule (LinkExtractor(restrict_xpaths = ('//span[@class="np"]')), callback = "parse_item", follow = True),)
 
     def parse_next_site(self, response):
-#        item = []
         item = response.request.meta['item']
         item['source_url'] = response.url
         item['source_page_body'] = response.body
@@ -39,9 +36,7 @@ class IndeedJpostingSpider(CrawlSpider):
     def parse_item(self, response):
         self.log('\n Crawling  %s\n' % response.url)
         hxs = Selector(response)
-#        sites = hxs.select("//div[@class='row ' or @class='row lastRow']")
         sites = hxs.xpath("//div[@class='  row  result' or @class='lastRow  row  result' or @class='row sjlast result' or @class='row  result']")
-	#sites = hxs.select("//div[@class='row ']")
         items = []
         for site in sites:
             item = IndeedItem(company='none')
@@ -52,15 +47,13 @@ class IndeedJpostingSpider(CrawlSpider):
             item['crawl_url'] = response.url
             item['location'] = site.xpath("span[@itemprop='jobLocation']//text()").extract()
 
-	    # Not all entries have a company
+	    # not all entries have a company name
             if site.xpath("span[@class='company']//text()").extract() == []:
                 item['company'] = [u'']
             else:
                 item['company'] = site.xpath("span[@class='company']//text()").extract()
                 item['summary'] = site.xpath("table//span[@class='summary']/text()").extract()
-#                item['src'] = site.xpath("table/tr/td/span[@class='source']/text()").extract()
                 item['found_date'] = site.xpath("table/tr/td//span[@class='date']/text()").extract()
-		#item['source_url'] = self.get_source(link_url)
                 request = Request("http://www.indeed.com" + item['link_url'][0], callback=self.parse_next_site)
                 request.meta['item'] = item
                 yield request
